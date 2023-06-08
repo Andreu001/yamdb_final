0ec47248c0d76dc68a940641f25fd7d2619c4989
Andreu001 <36473731+Andreu001@users.noreply.github.com>
2023-06-08 15:58:09 +0300

    zadolbalsya2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -13,7 +13,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['158.160.37.58', '127.0.0.1', 'localhost', 'web']
 
 
-# Application definition test 08/06/2023
+# Application definition test 08/06/2023 new
 
 INSTALLED_APPS = [
     'django.contrib.admin',
