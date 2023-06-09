95868cffdb247b27080a66ddd7d97a1bea38046b
Andreu001 <36473731+Andreu001@users.noreply.github.com>
2023-06-09 21:12:32 +0300

    zadolbalsya2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -108,8 +108,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
